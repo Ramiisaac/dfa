c24b9d730c3315cab847200b2ee593586bf48fba
Rami <isaacr@spu.edu>
2021-03-21 19:46:38 -0700

    added examples

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,12 @@ router.get('/navigation.html', function(req, res, next){
   res.sendFile('navigation.html', {root: './public/site'})
 });
 
+router.get('/ex2.html', function(req, res, next){
+  res.sendFile('ex2.html', {root: './public/site'})
+});
+
+router.get('/ex3.html', function(req, res, next){
+  res.sendFile('ex3.html', {root: './public/site'})
+});
+
 module.exports = router;
